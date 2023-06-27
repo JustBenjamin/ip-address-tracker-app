37798f8675d6f650a1fdc07383e6a7a8bb6beb13
Benjamin Hove <benhove86@gmail.com>
2023-06-27 15:24:39 -0400

    Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
+import arrow from './images/icon-arrow.svg';
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{  color: "#fff",
+                    fontSize: "1.3rem" }}>IP Address Tracker</h1>
+      <form>
+        <label>
+        <input type="text" />
+        <button type="button"><img src={arrow}  alt=""/></button>
+        </label>
+      </form>
+      <div className="output">
+        <div>
+          <p>IP ADDRESS</p>
+          <span></span>
+        </div>
+
+        <div>
+          <p>LOCATION</p>
+          <span></span>
+        </div>
+
+        <div>
+          <p>TIMEZONE</p>
+          <span></span>
+        </div>
+
+        <div>
+          <p>ISP</p>
+          <span></span>
+        </div>
+
+      </div>
     </div>
   );
 }
